4e73b071cd3bfdda904db19277cbde2ddc9a4f04
Fernando Sipaque <josefperez84@gmail.com>
2025-03-21 22:44:41 -0600

    Commit inicial

---
+++ src/main/java/umg/edu/gt/desarrollo/estructuradedatos2025/EstructuraDeDatos2025.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package umg.edu.gt.desarrollo.estructuradedatos2025;
+
+import java.util.Arrays;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+import umg.edu.gt.desarrollo.Utilidades.CompararStrings;
+import umg.edu.gt.desarrollo.estructuradedatos2025.clases.ClaseVectores;
+import umg.edu.gt.desarrollo.estructuradedatos2025.ejercicios.EjercicioVectores;
+
+/**
+ *
+ * @author wcordova
+ */
+public class EstructuraDeDatos2025 {
+	
+	private static final Logger logger = Logger.getLogger(EstructuraDeDatos2025.class);
+	
+
+    public static void main(String[] args) {
+    	logger.info("Bienvenidos a Programacion 3");
+    }
+    
+
+}
